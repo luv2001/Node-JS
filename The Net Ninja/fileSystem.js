47d83b1207ad22b4010e850f0eab1ae4df4911f4
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-08-31 14:06:51 +0530

    fs module added

--- The Net Ninja/fileSystem.js
+++ The Net Ninja/fileSystem.js
@@ -1,14 +1,29 @@
 const fs = require('fs')
 
-
-
 // Read Files 
-fs.readFile('./docs/fs-module.txt', (err, data) => {
-    if (err) {
-        console.log(err)
-    }
-    else {
-        console.log(data.toString())
-    }
-})
-console.log("oi This is Executed ==> Async Javscript")
+// fs.readFile('./docs/fs-module.txt', (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     else {
+//         console.log(data.toString())
+//     }
+// })
+// console.log("oi This is Executed ==> Async Javscript")
+
+// Writing from the file 
+// fs.writeFile('./docs/fs-module.txt', 'Tere naam se jee luun Ter naam se mar jaun', (error) => {
+//     if (error) { console.log(error) }
+//     console.log("File changed")
+// })
+
+// directiories
+// fs.mkdir("./componentss", (error) => { console.log(error) });
+
+// if (!fs.existsSync("./compo")) {
+//     fs.mkdir("./compo", () => { })
+// }
+
+if (fs.existsSync("./docs/deleteme.txt")) {
+    fs.unlink("./docs/deleteme.txt", () => { }) // delete files
+}
