e58faec85bcea7b8a3a5f9360469848121fa32c9
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-09-01 19:08:55 +0530

    basic routing added NODE JS (reading from HTML file + Client + server)

--- The Net Ninja/03-NodeJs-Client-Server/server.js
+++ The Net Ninja/03-NodeJs-Client-Server/server.js
@@ -1,15 +1,44 @@
 const http = require('http');
+const fs = require('fs');
+const port = 8000;
 
-const listenNa = http.createServer(function (req, res) {
-    res.writeHead("It is running")
-    res.end()
+const server = http.createServer(function (req, res) {
+    res.setHeader('Content-Type', 'text/html');
+
+
+    var url = req.url
+
+    if (url === "/") {
+        res.write("Don't be shy This is home page")
+        res.end();
+    } else
+
+        if (url === "/about") {
+            res.write("Don't be shy This is about ABOUT ABOUT")
+            res.end();
+        }
+        else {
+            fs.readFile('./routing/404.html', (error, response) => {
+                if (error) { console.error(`error aa rahi hai 404 wale mein`) }
+                else {
+                    res.write(response)
+                    res.end()
+                }
+            })
+
+
+        }
+
+
+    // fs.readFile('./index.html', (err, data) => {
+    //     if (err) { console.error(`err`); }
+    //     res.write(data)
+    //     res.end()
+    // })
+
+
 })
 
-listenNa.listen(3001, function (err, res) {
-    if (err) {
-        console.log(`There is some error`)
-    }
-    else {
-        console.log(` It is runnig on port 3001 `)
-    }
+server.listen(port, 'localhost', () => {
+    console.log(`Gand Mari hui hai Meri puri Gand Mari hui hai`)
 })
