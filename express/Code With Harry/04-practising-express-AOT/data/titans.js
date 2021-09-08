1dcfbf3b3ecb171ba741557bf3410ac43ad9ddbb
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-09-08 20:26:43 +0530

    Handlebars {{each}} this functionality added

---
+++ express/Code With Harry/04-practising-express-AOT/data/titans.js
@@ -0,0 +1,21 @@
+titans = [
+    {
+        name: "attack-titan",
+        currentHolder: "Eren Yeager",
+        slug: "attack-titan"
+    },
+    {
+        name: "female-titan",
+        currentHolder: "Anne",
+        slug: "female-titan"
+    },
+    {
+        name: "beast-titan",
+        currentHolder: "Zeke Yeager",
+        slug: "beast-titan"
+    }
+
+
+]
+
+module.exports = titans;
---
+++ express/Code With Harry/04-practising-express-AOT/routes/router.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const path = require('path');
+
+const titans = require('../data/titans')
+
+router.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, "../templates/index.html"))
+    res.render("home")
+})
+
+router.get("/titan-list", (req, res) => {
+    res.render("titan-list", {
+        titans: titans
+    })
+})
+
+router.get('/titans/:slug', (req, res) => {
+    const requireTitan = titans.filter(e => {
+        return e.slug === req.params.slug;
+    })
+    const titanSlug = requireTitan[0].slug + ".html"
+    res.sendFile(path.join(__dirname, `../templates/Titans/${titanSlug}`))
+})
+
+module.exports = router;
