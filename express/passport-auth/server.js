32eae83e525e7fe2f959adb23e39cc3d000bda36
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-06 12:16:39 +0530

    Login Register urlencoded

--- express/passport-auth/server.js
+++ express/passport-auth/server.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.render("./index.ejs")
+    res.render("./index.ejs", { name: "Luv" })
+})
+
+app.get('/login', (req, res) => {
+    res.render("./login.ejs")
+})
+
+app.get('/register', (req, res) => {
+    res.render("./register.ejs")
+})
+
+app.post('/register', (req, res) => {
+
 })
 
 app.listen(3000, () => {
